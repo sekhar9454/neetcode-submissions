980256ed4d0a6c48bb1a1d109a101bf76b501aac
Sanskar Sharma <sharmasanskar7201@gmail.com>
2026-07-02 13:21:20 +0530

    Add: count-paths - submission-1

---
+++ Data Structures & Algorithms/count-paths/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        int N = m + n - 2;
+        int R = min(m - 1, n - 1);
+
+        long long ans = 1;
+
+        for (int i = 1; i <= R; i++) {
+            ans = ans * (N - R + i) / i;
+        }
+
+        return (int)ans;
+    }
+};
